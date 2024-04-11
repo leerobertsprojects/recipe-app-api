e6b5c881e39ac070e18bdad7462c7017d9c375a3
lee roberts <lee.roberts@nowhere.com>
2024-04-11 16:00:42 +0100

    added pycopg and required dependencies to the Dockerfile and requirements.txt

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """ Add X & y and return result """
+    return x + y
+
+def subtract(x, y):
+    ''' Subtract X & Y and return the result'''
+    return x - y
